694170a483640d32d089c7408e88fb2a28ef2b4e
Michal Stabrawa <mistaaqua@gmail.com>
2021-10-30 21:00:24 +0200

    add excercise 7,8

--- name.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    name: "Michal",
-    surname: "Stabrawa"
-}
--- pb.js
+++ pb.js
@@ -1,16 +1,42 @@
 const os = require("os");
 const fs = require("fs");
 
-const example = require("./multiple");
+const math = require("./math");
 
-let nameUsers = os.userInfo();
-console.log(nameUsers.username);
+//7
+let nameUsers = os.userInfo(); //get user
+let getUserName = `Uzytkownik ${nameUsers.username}`;
 
-let readA = fs.readFileSync("./a.txt").toString();
+fs.writeFileSync("wynik.txt", getUserName + "\n");
+
+//8
+
+let readA = fs.readFileSync("./a.txt").toString(); //get a value with a.txt
+let readB = fs.readFileSync("./b.txt").toString(); //get b value with b.txt
 console.log(`readA to : ${parseInt(readA)}`);
 
-let readB = fs.readFileSync("./b.txt").toString();
+let addVariables = `Wynik ${readA} + ${readB} = ${math.add(
+  parseInt(readA),
+  parseInt(readB)
+)}`;
+let oddVariables = `Wynik ${readA} - ${readB} = ${math.odd(
+  parseInt(readA),
+  parseInt(readB)
+)}`;
+let multipleVariables = `Wynik ${readA} * ${readB} = ${math.multi(
+  parseInt(readA),
+  parseInt(readB)
+)}`;
+let divideVariables = `Wynik ${readA} / ${readB} = ${math.divide(
+  parseInt(readA),
+  parseInt(readB)
+)}`;
 
-console.log(example.add(parseInt(readA), parseInt(readB)));
+console.log(addVariables);
+console.log(oddVariables);
+console.log(multipleVariables);
+console.log(divideVariables);
 
-fs.writeFileSync("wynik.txt", nameUsers.username);
+fs.appendFileSync("wynik.txt", addVariables + "\n");
+fs.appendFileSync("wynik.txt", oddVariables + "\n");
+fs.appendFileSync("wynik.txt", divideVariables + "\n");
